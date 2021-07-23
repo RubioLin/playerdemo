d258acb10a34a18bd934c91a1473ec40286ae8ed
RubioLin <capricorn1230maple@gmail.com>
2021-07-23 15:54:49 +0800

    Initial Commit

---
+++ playerdemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  playerdemo
+//
+//  Created by 林俊緯 on 2021/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
